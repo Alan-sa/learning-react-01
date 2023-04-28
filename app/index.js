56d97d3bf03ce93f8bd2ab7d4bf011847757d513
ALSA <alan-sa@outlook.com>
2023-04-28 07:13:24 -0600

    update

--- app/index.js
+++ app/index.js
@@ -3,30 +3,43 @@ import { View, ScrollView, SafeAreaView } from "react-native";
 import { Stack, useRouter } from "expo-router";
 
 import { COLORS, icons, images, SIZES } from "../constants";
-import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-      <Stack.Screen options={{
-        headerStyle: {backgroundColor: COLORS.lightWhite},
-        headerShadowVisible: false,
-        headerLeft: () => (
-          <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
-        ),
-        headerRight: () => (
-          <ScreenHeaderBtn iconUrl={icons.profile} dimension="60%" />
-        ),
-        headerTitle: ""
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="60%" />
+          ),
+          headerTitle: "",
         }}
       />
-
-<ScrollView showsVerticalScrollIndicator={false}>
-  
-  </ScrollView>
-
-      </SafeAreaView>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            paddingHorizontal: SIZES.padding,
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
-}
+};
 
 export default Home;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import useState from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
 
-import styles from './welcome.style'
+export const jobTypes = ["Full Time", "Part Time", "Internship", "Freelance"];
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Alan</Text>
+        <Text style={styles.welcomeMessage}>Find your dream job</Text>
+      </View>
 
-export default Welcome
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="Search for job"
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
